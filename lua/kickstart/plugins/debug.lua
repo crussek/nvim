5f3e18cbdc768dda4bbe287dffedb5ccc6a35122
crussek <crussek92@gmail.com>
2025-07-10 09:48:57 -0400

    Get python debug working, kinda

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -23,7 +23,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
-    'microsoft/debugpy',
+    'mfussenegger/nvim-dap-python',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -96,6 +96,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'python',
       },
     }
 
@@ -145,5 +146,6 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+    require('dap-python').setup()
   end,
 }
