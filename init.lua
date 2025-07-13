2a784534bd59370205ba545a8d5c439c294932c9
crussek <crussek92@gmail.com>
2025-07-13 17:13:15 -0400

    Added some python sepcific breakpoints

--- init.lua
+++ init.lua
@@ -233,6 +233,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Breakpoints for Python
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    local function keymap_set(key, command, desc)
+      vim.keymap.set('n', '<leader>' .. key, 'O' .. command .. '; pass  # XXX: BREAKPOINT<C-c>', { buffer = true, desc = desc .. ' breakpoint' })
+    end
+
+    keymap_set(',', 'import ipdb; ipdb.set_trace()', 'IPDB')
+    keymap_set('<', 'from celery.contrib import rdb; rdb.set_trace()', 'RDB')
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
